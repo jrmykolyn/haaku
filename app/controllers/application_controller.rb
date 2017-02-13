e72dff5e6ea3efef15e9161aca9854cf16f2d315
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-02-13 18:22:50 -0500

    Completed first pass of app-wide 'redirect if not logged in' logic.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :redirect_to_login_if_required
+
+  def redirect_to_login_if_required
+      # TODO[@jrmykolyn]
+      # - decouple logic/condition from Devise controller.
+      if !current_user && ( params[ :controller ] != 'devise/sessions' )
+          redirect_to( login_path )
+      end
+  end
 end
