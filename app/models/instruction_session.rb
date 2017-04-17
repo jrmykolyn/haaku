076e0c49fb77da9a85a1cf8ffbbe47ef1277e97e
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-04-16 23:30:24 -0400

    Updated 'InstructionSessionNote' model: added 'instructor' attr. (alias of 'user').

--- app/models/instruction_session.rb
+++ app/models/instruction_session.rb
@@ -1,5 +1,6 @@
 class InstructionSession < ActiveRecord::Base
 	belongs_to :user
+	alias_attribute :instructor, :user # *ALLOW* `User` instance to be accessed via *EITHER* the `.instructor` or `.user` attrs.
 	has_many :attendance_records
 	has_many :students, through: :attendance_records
 	has_many :notes, foreign_key: 'instruction_session_id', class_name: 'InstructionSessionNote' # *REQUIRE* `InstructionSessionNote` instances to be accessed via `.notes` attr.
