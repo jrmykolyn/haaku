6498e328fe4c465ab56b915a5cc59431b3adb505
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-04-16 23:13:56 -0400

    Roughed out logic and templates for 'InstructionSessionNote' 'new' and 'show' pages.

--- app/controllers/instruction_session_notes_controller.rb
+++ app/controllers/instruction_session_notes_controller.rb
@@ -1,7 +1,32 @@
 class InstructionSessionNotesController < ApplicationController
-    
+
     def index
         @notes = InstructionSessionNote.all
     end
-    
+
+    def new
+        @session = InstructionSession.find( params[ :instruction_session_id ] )
+        @note = InstructionSessionNote.new
+    end
+
+    def create
+        @session = InstructionSession.find( params[ :instruction_session_id ] )
+        @note = InstructionSessionNote.new( instruction_session_note_params( params ) )
+
+        if @note.save
+            redirect_to instruction_session_instruction_session_note_path( @session, @note )
+        else
+            render :new
+        end
+    end
+
+    def show
+        @note = InstructionSessionNote.find( params[ :id ] )
+    end
+
+    private
+    def instruction_session_note_params( params )
+        return params.require( :instruction_session_note ).permit( :instruction_session_id, :title, :text )
+    end
+
 end
